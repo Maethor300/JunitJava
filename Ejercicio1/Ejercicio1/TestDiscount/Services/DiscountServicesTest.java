163c4e13a6e0ede3956f68e7ecd2fbd268bc7169
David Mendoza <davidleomendoza300@gmail.com>
2023-07-24 18:20:46 -0500

    update

--- Ejercicio1/Ejercicio1/TestDiscount/Services/DiscountServicesTest.java
+++ Ejercicio1/Ejercicio1/TestDiscount/Services/DiscountServicesTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 
 public class DiscountServicesTest {
      DiscountServices rs;
+
      @Before
      public void beforeInstance(){
        rs = new DiscountServices();
---
+++ Ejercicio2/TestTemperatureConverter/TemperatureConverter/TemperatureConverterTest.java
@@ -0,0 +1,8 @@
+package TemperatureConverter;
+
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+
+}
